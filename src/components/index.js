8c2efc54ceaca846ac0dd0ba1192f9775405924c
Ajulll22 <ajulrizki@gmail.com>
2022-08-30 13:31:24 +0700

    import components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Hasil from "./Hasil";
+import ListCategories from "./ListCategories";
+import Menus from "./Menus";
+import NavComponents from "./NavComponents";
+
+export { Hasil, ListCategories, Menus, NavComponents }
