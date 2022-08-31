0f21f5f76eb52d1554ad4b313e287c912121ca61
Ajulll22 <ajulrizki@gmail.com>
2022-08-31 20:32:08 +0700

    update margin

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -39,7 +39,7 @@ export default class ListCategories extends Component {
     const { categories } = this.state
     const { changeCategory, dipilih } = this.props
     return (
-      <Col md={2} mt='2'>
+      <Col md={2} mt='2' className='mt-3'>
         <h4><strong>Daftar Kategori</strong></h4>
         <hr />
         <ListGroup>
