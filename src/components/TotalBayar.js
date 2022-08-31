8afe7f3dae599639fde3fcb25270e3acc865ad10
Ajulll22 <ajulrizki@gmail.com>
2022-08-31 20:32:39 +0700

    submit total

--- src/components/TotalBayar.js
+++ src/components/TotalBayar.js
@@ -3,29 +3,72 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react'
 import { Button, Col, Row } from 'react-bootstrap'
 import { numberWithCommas } from '../utils/utils';
+import axios from 'axios'
+import { API_URL } from '../utils/constants';
+
 
 export default class TotalBayar extends Component {
+    submitTotalBayar = (totalBayar) => {
+        const pesanan = {
+            total_bayar: totalBayar,
+            menus: this.props.keranjangs
+        }
+
+        axios.post(API_URL + "pesanans", pesanan).then((res) => {
+            this.props.history.push('/sukses')
+        })
+    };
+
     render() {
         const totalBayar = this.props.keranjangs.reduce(function (result, item) {
             return result + item.total_harga;
         }, 0);
 
         return (
-            <div className='fixed-bottom'>
-                <Row>
-                    <Col md={{ span: 3, offset: 9 }} className="px-4">
-                        <h4>
-                            Total Harga : <strong className="float-end ms-2"> Rp. {numberWithCommas(totalBayar)} </strong>
-                        </h4>
+            <>
+                {/* WEB */}
+                <div className='fixed-bottom d-none d-md-block'>
+                    <Row>
+                        <Col md={{ span: 3, offset: 9 }} className="px-4">
+                            <h4>
+                                Total Harga : <strong className="float-end ms-2"> Rp. {numberWithCommas(totalBayar)} </strong>
+                            </h4>
 
-                        <div className="d-grid gap-2 mb-3 mt-4">
-                            <Button variant="primary" size="lg">
-                                <FontAwesomeIcon icon={faShoppingCart} /> <strong>Bayar</strong>
-                            </Button>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
+                            <div className="d-grid gap-2 mb-3 mt-4">
+                                <Button
+                                    variant="primary"
+                                    size="lg"
+                                    onClick={() => this.submitTotalBayar(totalBayar)}
+                                >
+                                    <FontAwesomeIcon icon={faShoppingCart} /> <strong>Bayar</strong>
+                                </Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+
+
+                {/* MOBILE */}
+                <div className='d-sm-block d-md-none'>
+                    <Row>
+                        <Col md={{ span: 3, offset: 9 }} className="px-4">
+                            <h4>
+                                Total Harga : <strong className="float-end ms-2"> Rp. {numberWithCommas(totalBayar)} </strong>
+                            </h4>
+
+                            <div className="d-grid gap-2 mb-3 mt-4">
+                                <Button
+                                    variant="primary"
+                                    size="lg"
+                                    onClick={() => this.submitTotalBayar(totalBayar)}
+                                >
+                                    <FontAwesomeIcon icon={faShoppingCart} /> <strong>Bayar</strong>
+                                </Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </>
         )
     }
 }
