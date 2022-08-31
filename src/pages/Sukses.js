f2d17bbc8ebf3dad4bf428dd6da44437427bf870
Ajulll22 <ajulrizki@gmail.com>
2022-08-31 20:30:09 +0700

    update ui

--- src/pages/Sukses.js
+++ src/pages/Sukses.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react'
-import { Button, Image } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Button, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
 
 export default class Sukses extends Component {
+    componentDidMount() {
+        axios
+            .get(API_URL + "keranjangs")
+            .then((res) => {
+                const keranjangs = res.data;
+                console.log(keranjangs);
+                keranjangs.map(function (item) {
+                    return axios
+                        .delete(API_URL + "keranjangs/" + item.id)
+                        .then((res) => console.log(res))
+                        .catch((error) => console.log(error))
+                })
+            })
+            .catch((error) => {
+                console.log("Error yaa ", error);
+            });
+    }
+
     render() {
         return (
-            <div className='mt-4 text-center'>
-                <Image src='assets/images/sukses.png' width={500} />
+            <div className="mt-4 text-center">
+                <Image src="assets/images/sukses.png" width="500" />
                 <h2>Sukses Pesan</h2>
-                <p>Terimakasih Sudah Memesan</p>
-                <Button variant='primary' as={Link} to="/" >
+                <p>Terimakasih Sudah Memesan!</p>
+                <Button variant="primary" as={Link} to="/">
                     Kembali
                 </Button>
             </div>
-        )
+        );
     }
 }
