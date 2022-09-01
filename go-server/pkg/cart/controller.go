531470ab29c4406643da24543c56faa9d641cad6
Ajulll22 <ajulrizki@gmail.com>
2022-09-02 01:16:08 +0700

    route regis

---
+++ go-server/pkg/cart/controller.go
@@ -0,0 +1,20 @@
+package cart
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type handler struct {
+	DB *gorm.DB
+}
+
+func RegisterRoutes(app *fiber.App, db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+
+	routes := app.Group("/api/cart")
+	routes.Get("/", h.GetCart)
+	routes.Post("/", h.AddCart)
+}
