bdfd00cc58774f07c45f3c76a34e8326a294044c
Ajulll22 <ajulrizki@gmail.com>
2022-09-02 01:14:47 +0700

    table cart

--- go-server/models/cart.go
+++ go-server/models/cart.go
@@ -4,7 +4,8 @@ type Cart struct {
 	Id_cart     uint          `json:"id_cart" gorm:"primaryKey;type:int(10)"`
 	Jumlah      uint          `json:"jumlah" gorm:"type:int(10)"`
 	Total_harga uint          `json:"total_harga" gorm:"type:int(50)"`
+	Keterangan  string        `json:"keterangan" gorm:"type:varchar(75)"`
 	Status_cart bool          `json:"status_cart" gorm:"default:true"`
 	Id_product  uint          `json:"id_product"`
-	Transaction []Transaction `gorm:"foreignKey:Id_cart;references:Id_cart;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Transaction []Transaction `json:"-" gorm:"foreignKey:Id_cart;references:Id_cart;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
