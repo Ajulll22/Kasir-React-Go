2be9399d90aa473be018f1e34452fbf3bd263bd4
Ajulll22 <ajulrizki@gmail.com>
2022-09-01 16:37:33 +0700

    config viper

---
+++ go-server/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port  string `mapstructure:"PORT"`
+	DBUrl string `mapstructure:"DB_URL"`
+}
+
+func LoadConfig() (c Config, err error) {
+	viper.AddConfigPath("../config/env/")
+	viper.SetConfigName("conf")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&c)
+
+	return
+}
+
+const SecretKey = "secret"
