c9075afcca293d9e0f7aaeecc38c636f6898efda
Ajulll22 <ajulrizki@gmail.com>
2022-09-01 16:37:53 +0700

    table category

---
+++ go-server/models/category.go
@@ -0,0 +1,7 @@
+package models
+
+type Category struct {
+	Id_category   uint      `json:"id_category" gorm:"primaryKey;type:int(10)"`
+	Nama_category string    `json:"nama_category" gorm:"type:varchar(25)"`
+	Product       []Product `gorm:"foreignKey:Id_category;references:Id_category;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
