04b32d20192058554c877be6191537c2c6b06d1d
Ajulll22 <ajulrizki@gmail.com>
2022-09-02 01:16:33 +0700

    regis route

---
+++ go-server/pkg/category/controller.go
@@ -0,0 +1,19 @@
+package category
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type handler struct {
+	DB *gorm.DB
+}
+
+func RegisterRoutes(app *fiber.App, db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+
+	routes := app.Group("/api/categories")
+	routes.Get("/", h.GetCategory)
+}
