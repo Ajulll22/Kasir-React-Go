228721a3327ce41e793a380736574906177d1e53
Ajulll22 <ajulrizki@gmail.com>
2022-09-04 14:00:58 +0700

    update

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -5,12 +5,12 @@ import { numberWithCommas } from '../utils/utils'
 const Menus = ({ menu, masukKeranjang }) => {
     return (
         <Col md={4} xs={6} className="mb-4" >
-            <Card className='shadow' onClick={() => masukKeranjang(menu)} >
-                <Card.Img style={{ height: '14rem' }} variant="top" src={"assets/images/" + menu.category.nama.toLowerCase() + "/" + menu.gambar} />
+            <Card className='shadow' onClick={() => masukKeranjang(menu.id_product)} >
+                <Card.Img style={{ height: '14rem' }} variant="top" src={"assets/images/" + menu.category.nama_category.toLowerCase() + "/" + menu.gambar_product} />
                 <Card.Body>
-                    <Card.Title>{menu.nama}</Card.Title>
+                    <Card.Title>{menu.nama_product}</Card.Title>
                     <Card.Text>
-                        Rp. {numberWithCommas(menu.harga)}
+                        Rp. {numberWithCommas(menu.harga_product)}
                     </Card.Text>
                 </Card.Body>
             </Card>
