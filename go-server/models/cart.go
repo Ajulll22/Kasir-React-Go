6f978e62b4006b4e9149884205bc43cc8e5e47fc
Ajulll22 <ajulrizki@gmail.com>
2022-09-06 22:21:49 +0700

    update realasi

--- go-server/models/cart.go
+++ go-server/models/cart.go
@@ -1,11 +1,11 @@
 package models
 
 type Cart struct {
-	Id_cart     uint          `json:"id_cart" gorm:"primaryKey;type:int(10)"`
-	Jumlah      uint          `json:"jumlah" gorm:"type:int(10)"`
-	Total_harga uint          `json:"total_harga" gorm:"type:int(50)"`
-	Keterangan  string        `json:"keterangan" gorm:"type:varchar(75)"`
-	Status_cart bool          `json:"status_cart" gorm:"default:true"`
-	Id_product  uint          `json:"id_product"`
-	Transaction []Transaction `json:"-" gorm:"foreignKey:Id_cart;references:Id_cart;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Id_cart     int     `json:"id_cart" gorm:"primaryKey;type:int(10)"`
+	Jumlah      int     `json:"jumlah" gorm:"type:int(10)"`
+	Total_harga int     `json:"total_harga" gorm:"type:int(50)"`
+	Keterangan  string  `json:"keterangan" gorm:"type:varchar(75)"`
+	Status_cart bool    `json:"status_cart" gorm:"default:true"`
+	Id_product  int     `json:"id_product"`
+	Product     Product `gorm:"foreignKey:Id_product;references:Id_product;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"product,omitempty"`
 }
